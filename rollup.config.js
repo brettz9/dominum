7ee30d442bb79368e5aed81ce6b757f55547eccf
Brett Zamir <brettz9@yahoo.com>
2020-01-14 11:42:11 +0800

    - Add Rollup

---
+++ rollup.config.js
@@ -0,0 +1,47 @@
+// import babel from 'rollup-plugin-babel';
+// import {terser} from 'rollup-plugin-terser';
+
+// We don't need this so long as we are hard-coding the
+//  `node_modules` path for the sake of the browser, but keeping
+//  in event we can use import paths later
+import nodeResolve from '@rollup/plugin-node-resolve';
+
+/**
+ * @external RollupConfig
+ * @type {PlainObject}
+ * @see {@link https://rollupjs.org/guide/en#big-list-of-options}
+ */
+
+/**
+ * @param {PlainObject} [config= {}]
+ * @param {boolean} [config.minifying=false]
+ * @param {string} [config.format='umd']
+ * @returns {external:RollupConfig}
+ */
+function getRollupObject ({minifying, format = 'umd'} = {}) {
+  const nonMinified = {
+    input: 'src/index.js',
+    output: {
+      format,
+      sourcemap: minifying,
+      file: `dist/index.${format}${minifying ? '.min' : ''}.js`,
+      name: 'Dominum'
+    },
+    plugins: [
+      // babel(),
+      nodeResolve()
+    ]
+  };
+  if (minifying) {
+    // nonMinified.plugins.push(terser());
+  }
+  return nonMinified;
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  // getRollupObject({minifying: true, format: 'umd'}),
+  getRollupObject({minifying: false, format: 'umd'}),
+  // getRollupObject({minifying: true, format: 'esm'}),
+  getRollupObject({minifying: false, format: 'esm'})
+];

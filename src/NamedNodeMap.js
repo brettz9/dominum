6350dd529c3f1a05a9cb3daea7a6a31b06753452
Brett Zamir <brettz9@yahoo.com>
2020-01-15 08:51:09 +0800

    `NamedNodeMap` comment

--- src/NamedNodeMap.js
+++ src/NamedNodeMap.js
@@ -1,5 +1,10 @@
 // Todo (high): Add (standard?) way to populate and retrieve (and remove
 //   `_namedNodeMapArrayImpl` as export?)
+
+/**
+ * This class is "live", meaning its `length` is dynamic
+ * as items are added to the list.
+ */
 class NamedNodeMap {
   constructor () {
     this.length = 0;

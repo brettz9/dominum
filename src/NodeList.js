3d1ea2731e830b6a11056a07f1e02d352585536d
Brett Zamir <brettz9@yahoo.com>
2020-01-15 08:50:19 +0800

    - NodeList comment; add `ignore` directory

--- src/NodeList.js
+++ src/NodeList.js
@@ -1,4 +1,10 @@
 // Todo (high): Add way to populate and retrieve
+
+/**
+ * This class is of the "live" `NodeList` variety (unlike say the `NodeList`
+ * given by `document.querySelectorAll`), meaning its `length` is dynamic
+ * as items are added to the list.
+ */
 class NodeList {
   constructor () {
     this.length = 0;

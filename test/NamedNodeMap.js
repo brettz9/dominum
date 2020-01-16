8587e4440aa1cc67c7d7c4b4872ebf17a16bb48c
Brett Zamir <brettz9@yahoo.com>
2020-01-16 10:46:15 +0800

    - Testing: Coverage setting non-index properties

--- test/NamedNodeMap.js
+++ test/NamedNodeMap.js
@@ -5,4 +5,10 @@ describe('NamedNodeMap', function () {
     const namedNodeMap = new NamedNodeMap();
     expect(namedNodeMap.length).to.equal(0);
   });
+  it('should allow setting other properties', function () {
+    const namedNodeMap = new NamedNodeMap();
+    namedNodeMap.test = 5;
+    expect(namedNodeMap.test).to.equal(5);
+    expect(namedNodeMap.length).to.equal(0);
+  });
 });
--- test/NodeList.js
+++ test/NodeList.js
@@ -2,7 +2,13 @@ import NodeList from '../src/NodeList.js';
 
 describe('NodeList', function () {
   it('should have `NodeList` properties (`length`)', function () {
-    const namedNodeMap = new NodeList();
-    expect(namedNodeMap.length).to.equal(0);
+    const nodeList = new NodeList();
+    expect(nodeList.length).to.equal(0);
+  });
+  it('should allow setting other properties', function () {
+    const nodeList = new NodeList();
+    nodeList.test = 5;
+    expect(nodeList.test).to.equal(5);
+    expect(nodeList.length).to.equal(0);
   });
 });
